07625262b9b0e9231cd1eba6c4fb88561ecaea53
laggingreflex <laggingreflex@gmail.com>
2018-12-22 16:30:04 +0530

    fix: empty filelist on first run sometimes
    Sometimes it takes more than the 100ms debounce delay on initial file finding run of chokidar, so there should be a separate `readyTimeout`
    
    chokidar has a 'ready' event but it has issues (sometimes it never fires)

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const { watch } = require('chokidar');
 const debounce = require('debounce-queue');
 const breakAI = require('break-async-iterator')
-const { Files, Defer } = require('./utils');
+const { Files, Defer, ...utils } = require('./utils');
 
 module.exports = breakAI(breakable => async function*(paths, chokidarOpts = {}, opts = {}) {
   const files = new Files();
   const deferred = new Defer();
   const yielded = new Defer();
 
+  const ready = debounce(() => {
+    ready.is = true;
+    deferred.resolve();
+    deferred.reset();
+  }, opts.readyTimeout || 1000);
+
   const on = debounce(queue => {
     queue.forEach(([event, file]) => {
       files.update(file, { event });
     });
-    deferred.resolve();
-    deferred.reset();
-    return yielded;
-  }, { delay: opts.debounce || 100 });
+    if (!ready.is) {
+      ready();
+    } else {
+      deferred.resolve();
+      deferred.reset();
+    }
+  }, opts.debounce || 100);
 
   const watcher = watch(paths, { ...chokidarOpts, });
   watcher.on('all', on);
